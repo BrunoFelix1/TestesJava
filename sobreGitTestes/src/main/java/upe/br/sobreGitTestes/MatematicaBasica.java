875a95ff07aee2e2ff24325b4b058b6136b0ccfc
Bruno Felix <brunofelixpinto@gmail.com>
2024-07-01 19:31:15 -0300

    Mais operações

--- sobreGitTestes/src/main/java/upe/br/sobreGitTestes/MatematicaBasica.java
+++ sobreGitTestes/src/main/java/upe/br/sobreGitTestes/MatematicaBasica.java
@@ -5,9 +5,30 @@ public class MatematicaBasica {
         int num1 = 13;
         int num2 = 17;
         System.out.println( num1 + " + " + num2 + " = " + add(num1, num2) );
+        System.out.println( num1 + " - " + num2 + " = " + subtract(num1, num2) );
+        System.out.println( num1 + " * " + num2 + " = " + multiplicate(num1, num2) );
+        System.out.println( num1 + " / " + num2 + " = " + divide(num1, num2) );
+        System.out.println( num1 + " % " + num2 + " = " + mod(num1, num2) );
     }
 
     public static int add (int a, int b) {
         return a+b;
     }
+
+    public static int subtract (int a, int b) {
+        return a-b;
+    }
+
+    public static int multiplicate (int a, int b) {
+        return a*b;
+    }
+
+    public static int divide (int a, int b) {
+        return a/b;
+    }
+
+    public static int mod (int a, int b) {
+        return a%b;
+    }
+
 }
--- sobreGitTestes/src/test/java/upe/br/sobreGitTestes/MatematicaBasicaTest.java
+++ sobreGitTestes/src/test/java/upe/br/sobreGitTestes/MatematicaBasicaTest.java
@@ -5,11 +5,47 @@ import org.junit.jupiter.api.Test;
 
 public class MatematicaBasicaTest {
     @Test
-    public void shouldAnswerWithTrue()
+    public void testAdd()
     {
         int num1 = 13;
         int num2 = 12;
         int esperado = 25;
         assertEquals(esperado, MatematicaBasica.add(num1,num2));
+    }
+
+    @Test
+    public void testSubtract()
+    {
+        int num1 = 13;
+        int num2 = 12;
+        int esperado = 1;
+        assertEquals(esperado, MatematicaBasica.subtract(num1,num2));
+    }
+
+    @Test
+    public void testMultiplicate()
+    {
+        int num1 = 2;
+        int num2 = 3;
+        int esperado = 6;
+        assertEquals(esperado, MatematicaBasica.multiplicate(num1,num2));
+    }
+
+    @Test
+    public void testDivide()
+    {
+        int num1 = 8;
+        int num2 = 4;
+        int esperado = 2;
+        assertEquals(esperado, MatematicaBasica.divide(num1,num2));
+    }
+
+    @Test
+    public void testMod()
+    {
+        int num1 = 5;
+        int num2 = 3;
+        int esperado = 2;
+        assertEquals(esperado, MatematicaBasica.mod(num1,num2));
     }
 }
